0bacdc089363a240b3d99187543373a7bd0a1f54
haroon <haroon@Dures-MacBook-Air.local>
2023-02-25 17:05:25 +0000

    Working for whole numbers

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
-const generatequestion = () => {
-    console.log('generatequestion button pressed');
-    let a = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
-    let b = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
-    let c = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+    // const a = parseInt(1) ;
+    // const b = parseFloat(-10);
+    // const c = parseFloat(5);
+
+    const a = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+    console.log(a)
+    if (a == 0) {
+        a = a + 1;
+    } 
+
+    const b = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+    const c = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+
+    const answer = (c-b)/a
+
+
+    console.log('answer is '+ answer);
+    console.log('does answer = 15?');
+    console.log(15==answer)
+    console.log("answer.type");
+    console.log(answer.type);
+    console.log("------------")
+
     console.log('a is '+ a);
+    console.log("a.type");
+    console.log(a.type);
+
+    console.log("------------")
     console.log('b is '+ b);
+    console.log("b.type");
+    console.log(b.type);
+
+    console.log("------------")
     console.log('c is '+ c); 
+    console.log("c.type");
+    console.log(c.type);
+
+    document.querySelector(".question").innerText = `
+    ${a}x + ${b} = ${c}
+    `
+
+    console.log("------------")
+
+const checkAnswer = (e) => {
+    const userGuess = parseFloat(document.getElementById("guess").value);
+    console.log('Check answer button pressed');
+    console.log("userGuess");
+    console.log(userGuess);
+    console.log(userGuess.type);
+    console.log("answer");
+    console.log(answer.type);
+    
+    if (userGuess == answer) {
+        document.querySelector(".result").innerText = `
+        Well done! Your answer is correct!`
+    } else {
+        document.querySelector(".result").innerText = `
+        The answer is incorrect, please try again`
+    }
+    
 };
