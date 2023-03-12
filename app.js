3d8e7b60d505df88d9117b20741a896a2ab2a04c
haroon <haroon@Dures-MacBook-Air.local>
2023-03-12 21:08:57 +0000

    indentation changed

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-    const a = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
-    console.log(a)
-    if (a == 0) {
-        a = a + 1;
-    } 
-    const b = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
-    const c = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+const a = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+console.log(a)
+if (a == 0) {
+    a = a + 1;
+} 
+const b = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+const c = Math.floor(Math.random() * (10 - -10 + 1)) + -10;
 
-    const answer = (c-b)/a
+const answer = (c-b)/a
 
-    document.querySelector(".question").innerText = `
-    ${a}x + ${b} = ${c}
-    `
+document.querySelector(".question").innerText = `
+${a}x + ${b} = ${c}
+`
 
 const checkAnswer = (e) => {
-    const userGuess = parseFloat(document.getElementById("guess").value);
-    
-    
-    if (userGuess.toFixed(2) == answer.toFixed(2)) {
-        document.querySelector(".incorrectresult").innerText = ``
-        document.querySelector(".correctresult").innerText = `
-        Well done! Your answer is correct!`
-    } else {
-        document.querySelector(".correctresult").innerText = ``
-        document.querySelector(".incorrectresult").innerText = `
-        The answer is incorrect, please try again`
-    }
-    
+const userGuess = parseFloat(document.getElementById("guess").value);
+
+
+if (userGuess.toFixed(2) == answer.toFixed(2)) {
+    document.querySelector(".incorrectresult").innerText = ``
+    document.querySelector(".correctresult").innerText = `
+    Well done! Your answer is correct!`
+} else {
+    document.querySelector(".correctresult").innerText = ``
+    document.querySelector(".incorrectresult").innerText = `
+    The answer is incorrect, please try again`
+}
+
 };
 
 const refreshButton = document.getElementById("refresh-btn");
-  refreshButton.addEventListener("click", function() {
-    location.reload();
-  });
+refreshButton.addEventListener("click", function() {
+location.reload();
+});
